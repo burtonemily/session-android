1ff31e08fe0e553b7e562e8bcb4136d4b782b495
Emily <92288602+burtonemily@users.noreply.github.com>
2023-08-01 11:51:58 +1000

    libsession folder

---
+++ libsession-util/.cxx/Debug/6s4d6l27/arm64-v8a/libsession/src/version.c
@@ -0,0 +1,10 @@
+#include <session/version.h>
+
+const uint16_t LIBSESSION_UTIL_VERSION[3] =
+    {0, 0, 1};
+
+const char* LIBSESSION_UTIL_VERSION_FULL =
+    "libsession-util v0.0.1-nogit";
+
+const char* LIBSESSION_UTIL_VERSION_STR =
+    "v0.0.1-nogit";
